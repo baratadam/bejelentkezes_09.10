2d8f6bf760727e3081729832df7a36c40e6b39fb
Martin4122 <dreamlobby717@gmail.com>
2025-09-12 17:21:18 +0200

    ellenorzesek, megjelenes

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ document.addEventListener("DOMContentLoaded", () => {
     return re.test(email);
   }
 });
+
+document.getElementById("showRegister").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginFormDiv.classList.add("hidden");
+    registerFormDiv.classList.remove("hidden");
+  });
+
+  document.getElementById("showLogin").addEventListener("click", (e) => {
+    e.preventDefault();
+    registerFormDiv.classList.add("hidden");
+    loginFormDiv.classList.remove("hidden");
+  });
+
+  loginFormDiv.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = document.getElementById("loginEmail").value.trim();
+    const password = document.getElementById("loginPassword").value.trim();
+
+    if (!validateEmail(email)) {
+      loginError.textContent = "Hibás email formátum!";
+      return;
+    }
+
+    if (password.length < 6) {
+      loginError.textContent = "A jelszónak legalább 6 karakter hosszúnak kell lennie!";
+      return;
+    }
+    loginError.textContent = "";
+    alert("Sikeres bejelentkezés!");
+});
