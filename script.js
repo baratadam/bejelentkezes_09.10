b0b7596d0cfb57d49b2b55de9747de6fe9e55274
Baráth Ádám <baratadam06@gmail.com>
2025-09-12 17:18:57 +0200

    haladas

--- script.js
+++ script.js
@@ -10,4 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
   }
+  
+  document.getElementById("showRegister").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginFormDiv.classList.add("hidden");
+    registerFormDiv.classList.remove("hidden");
+  });
+
+  
+  document.getElementById("showLogin").addEventListener("click", (e) => {
+    e.preventDefault();
+    registerFormDiv.classList.add("hidden");
+    loginFormDiv.classList.remove("hidden");
+  });
+
+
 });
